34fc6eee04e0fff800cf47ccb2500edb4691689d
Derek Hower <quic_dhower@quicinc.com>
2024-08-15 10:10:21 -0700

    Add IP/IE CSRs, fix up sign extension

---
+++ cfgs/qc_iu/arch_overlay/csr/Xqciu/gen_mcliciX.rb
@@ -0,0 +1,56 @@
+
+require 'erb'
+
+pend_template = <<~YAML
+  # yaml-language-server: $schema=../../../../../schemas/csr_schema.json
+
+  mclicip<%= num %>:
+    long_name: IRQ Pending <%= num %>
+    address: 0x<%= (0x7f0 + num).to_s(16) %>
+    length: 32
+    priv_mode: M
+    definedBy: Xqciu
+    description: |
+      Pending bits for IRQs <%= num*32 %>-<%= (num + 1)*32 - 1 %>
+    fields:
+      <%- 32.times do |i| -%>
+      IRQ<%= num*32 + i %>:
+        type: RW
+        reset_value: 0
+        location: <%= i %>
+        description: IRQ<%= num*32 + 1 %> pending
+      <%- end -%>
+YAML
+
+en_template = <<~YAML
+  # yaml-language-server: $schema=../../../../../schemas/csr_schema.json
+
+  mclicie<%= num %>:
+    long_name: IRQ Enable <%= num %>
+    address: 0x<%= (0x7f0 + num).to_s(16) %>
+    length: 32
+    priv_mode: M
+    definedBy: Xqciu
+    description: |
+      Enable bits for IRQs <%= num*32 %>-<%= (num + 1)*32 - 1 %>
+    fields:
+      <%- 32.times do |i| -%>
+      IRQ<%= num*32 + i %>:
+        type: RW
+        reset_value: 0
+        location: <%= i %>
+        description: IRQ<%= num*32 + 1 %> enabled
+      <%- end -%>
+YAML
+
+root = File.dirname(__FILE__)
+
+erb = ERB.new(pend_template, trim_mode: '-')
+8.times do |num|
+  File.write("#{root}/mclicip#{num}.yaml", erb.result(binding))
+end
+
+erb = ERB.new(en_template, trim_mode: '-')
+8.times do |num|
+  File.write("#{root}/mclicie#{num}.yaml", erb.result(binding))
+end
