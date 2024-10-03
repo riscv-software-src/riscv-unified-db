85ea0fe169e80d053da1b53d847ff8527a07d175
Derek Hower <quic_dhower@quicinc.com>
2024-10-03 06:46:54 -0700

    Add missing mock methods for IDL test

--- lib/idl/tests/helpers.rb
+++ lib/idl/tests/helpers.rb
@@ -11,15 +11,22 @@ class MockExtension
 end
 
 MockExtensionParameter = Struct.new(:name, :desc, :schema, :extra_validation, :exts, :type)
+MockExtensionParameterWithValue = Struct.new(:name, :desc, :schema, :extra_validation, :exts, :value)
 
 # ArchDef mock that knows about XLEN and extensions
 class MockArchDef
   def param_values = { "XLEN" => 32 }
+  def params_with_value = [MockExtensionParameterWithValue.new("XLEN", "mxlen", {"type" => "integer", "enum" => [32, 64]}, nil, nil, 32)]
+  def params_without_value = []
   def params = []
   def extensions = [MockExtension.new("I")]
   def mxlen = 64
   def exception_codes = [OpenStruct.new(var: "ACode", num: 0), OpenStruct.new(var: "BCode", num: 1)]
   def interrupt_codes = [OpenStruct.new(var: "CoolInterrupt", num: 1)]
+
+  def fully_configured? = false
+  def partially_configured? = true
+  def unconfigured? = false
 end
 
 module TestMixin
