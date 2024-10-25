f68d5961b1f92ac0647d41984eeca68f7b64f828
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2024-10-25 14:02:13 -0700

    indexing all yaml/json files recurcively

--- index-unifieddb.js
+++ index-unifieddb.js
@@ -5,50 +5,23 @@ const process = require('process');
 const path = require('path');
 const fs = require('fs');
 
-const { readdir } = fs.promises;
+const { readdir, stat } = fs.promises;
 
-const tree = {
-  arch: {
-    inst: { // TODO update this object to index new extension
-      A: {},
-      B: {},
-      F: {},
-      H: {},
-      I: {},
-      M: {},
-      Q: {},
-      S: {},
-      Svinval: {},
-      V: {},
-      Zabha: {},
-      Zacas: {},
-      Zalasr: {},
-      Zawrs: {},
-      Zfbfmin: {},
-      Zfh: {},
-      Zicbom: {},
-      Zicboz: {},
-      Zicfiss: {},
-      Zicond: {},
-      Zicsr: {},
-      Zifencei: {}
-    }
-  }
-};
-
-const rec = async (node, branch, root) => {
-  const keys = Object.keys(node);
-  if (keys.length === 0) {
-    const fullPath = path.resolve(root, ...branch);
-    const files = await readdir(fullPath);
-    for (const file of files) {
-      const baseName = path.basename(file, '.yaml');
-      node[baseName] = {$ref: path.join(...branch, file)};
-    }
-  } else {
-    for (const key of keys) {
-      const newBranch = [...branch, key];
-      await rec(node[key], newBranch, root);
+const rec = async (branch, root) => {
+  const node = {};
+  const localPath = path.resolve(root, ...branch);
+  const els = await readdir(localPath);
+  for (const el of els) {
+    const isFile = (await stat(path.resolve(localPath, el))).isFile();
+    const fileExt = path.extname(el);
+    const baseName = path.basename(el, fileExt);
+    console.log(localPath, el, baseName, fileExt, isFile);
+    if (isFile) {
+      if (['.yaml', '.json'].includes(fileExt)) {
+        node[baseName] = {$ref: path.join(...branch, el)};
+      }
+    } else {
+      node[el] = await rec([...branch, el], root);
     }
   }
   return node;
@@ -57,11 +30,11 @@ const rec = async (node, branch, root) => {
 const main = async () => {
   const [, , root] = process.argv;
   if (root === undefined) {
-    console.error('usage: ./index-unifieddb.js <path-to-unifieddb-root>');
+    console.error('usage: ./index-unifieddb.js <path-to-unifieddb-arch-folder>');
     return;
   }
   const rootPath = path.resolve('.', root);
-  await rec(tree, [], rootPath);
+  const tree = await rec([], rootPath);
   console.log(JSON.stringify(tree, null, 2));
 };
 
