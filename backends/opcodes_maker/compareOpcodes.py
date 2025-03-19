72c7a7f5d018265f9e09ab28cb6f12b2055d7743
Afonso Oliveira <Afonso.Oliveira@synopsys.com>
2025-03-19 18:04:29 +0000

    Enhance testing method to split differences. Added warnings instead of errors when UDB has more info than riscv-opcodes.
    Signed-off-by: Afonso Oliveira <Afonso.Oliveira@synopsys.com>

--- backends/opcodes_maker/compareOpcodes.py
+++ backends/opcodes_maker/compareOpcodes.py
@@ -1,5 +1,6 @@
 import json
 import pytest
+import warnings
 
 
 def load_json_file(filename):
@@ -11,8 +12,12 @@ def load_json_file(filename):
 JSON1 = load_json_file("sorted_instr_dict.json")
 JSON2 = load_json_file("../../ext/riscv-opcodes/instr_dict.json")
 
+# Create lowercase versions of the keys for comparison
+JSON1_LOWER = {k.lower(): v for k, v in JSON1.items()}
+JSON2_LOWER = {k.lower(): v for k, v in JSON2.items()}
+
 # Compute the common instructions for parametrization.
-COMMON_INSTRUCTIONS = sorted(list(set(JSON1.keys()) & set(JSON2.keys())))
+COMMON_INSTRUCTIONS = sorted(list(set(JSON1_LOWER.keys()) & set(JSON2_LOWER.keys())))
 
 
 @pytest.fixture
@@ -25,33 +30,54 @@ def json2():
     return JSON2
 
 
-def test_instructions_presence(json1, json2):
+@pytest.fixture
+def json1_lower():
+    return JSON1_LOWER
+
+
+@pytest.fixture
+def json2_lower():
+    return JSON2_LOWER
+
+
+def test_instructions_missing_in_riscv_opcodes(json1_lower, json2_lower):
     """
-    Ensure that both JSON files contain the same instructions.
+    Check for instructions in sorted_instr_dict.json but missing in riscv-opcodes.
+    This will issue a warning instead of failing.
     """
-    instructions1 = set(json1.keys())
-    instructions2 = set(json2.keys())
+    instructions1 = set(json1_lower.keys())
+    instructions2 = set(json2_lower.keys())
 
     missing_in_file2 = instructions1 - instructions2
+
+    if missing_in_file2:
+        warnings.warn(f"Instructions missing in riscv-opcodes: {missing_in_file2}")
+
+
+def test_instructions_missing_in_sorted_dict(json1_lower, json2_lower):
+    """
+    Check for instructions in riscv-opcodes but missing in sorted_instr_dict.json.
+    This will fail the test if any are found.
+    """
+    instructions1 = set(json1_lower.keys())
+    instructions2 = set(json2_lower.keys())
+
     missing_in_file1 = instructions2 - instructions1
 
-    assert (
-        missing_in_file2 == set()
-    ), f"Instructions missing in '../../ext/riscv-opcodes/instr_dict.json': {missing_in_file2}"
     assert (
         missing_in_file1 == set()
     ), f"Instructions missing in 'sorted_instr_dict.json': {missing_in_file1}"
 
 
 @pytest.mark.parametrize("instr", COMMON_INSTRUCTIONS)
-def test_variable_fields_exact(instr, json1, json2):
+def test_variable_fields_exact(instr, json1_lower, json2_lower):
     """
     For each common instruction, verify that both JSON files:
       - Have the key 'variable_fields'
       - Contain an identical set of variable_fields (order does not matter)
     """
-    entry1 = json1[instr]
-    entry2 = json2[instr]
+    entry1 = json1_lower[instr]
+    entry2 = json2_lower[instr]
 
     assert (
         "variable_fields" in entry1
