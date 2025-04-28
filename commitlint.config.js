16dd243bb1509b29cb5dfd2b370c3afb1c1d63a6
Derek Hower <dhower@qti.qualcomm.com>
2025-04-28 08:37:12 -0700

    docs: add license info to readme files

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
 const Configuration = {
   extends: ["@commitlint/config-conventional"]
 };
