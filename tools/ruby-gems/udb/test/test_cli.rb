d96e2342aaf977a35ac1efe57692be66fbdb8c75
Derek Hower <dhower@qti.qualcomm.com>
2025-08-07 14:19:15 -0700

    fix: writable key in debug csrs

--- tools/ruby-gems/udb/test/test_cli.rb
+++ tools/ruby-gems/udb/test/test_cli.rb
@@ -1,3 +1,4 @@
+# typed: false
 # Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 # SPDX-License-Identifier: BSD-3-Clause-Clear
 
@@ -70,7 +71,6 @@ class TestCli < Minitest::Test
     assert_empty err
     repo_top = Udb.repo_root
     num_csr_yaml_files = `find #{repo_top}/spec/std/isa/csr/ -name '*.yaml' | wc -l`.to_i
-    puts num_csr_yaml_files
     assert_equal num_csr_yaml_files, out.split.length
   end
 
