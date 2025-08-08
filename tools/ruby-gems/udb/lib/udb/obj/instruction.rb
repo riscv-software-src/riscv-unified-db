e6ef784928c9ce1ee09e6321a7b61f17e9c0118b
James Ball <jameball@qti.qualcomm.com>
2025-08-08 17:36:05 +0000

    fix: debug puts left in output (commented out)

--- tools/ruby-gems/udb/lib/udb/obj/instruction.rb
+++ tools/ruby-gems/udb/lib/udb/obj/instruction.rb
@@ -510,7 +510,7 @@ class Instruction < TopLevelDatabaseObject
         else
           effective_xlen = cfg_arch.mxlen
           pruned_ast = pruned_operation_ast(effective_xlen)
-          puts " #{name}..."
+          # puts " #{name}..."
           symtab = fill_symtab(effective_xlen, pruned_ast)
           e = mask_to_array(pruned_ast.reachable_exceptions(symtab)).map { |code|
             etype.element_name(code)
