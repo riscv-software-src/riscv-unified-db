49093dc22e4b28fc03414507df9b792009cd6328
Debanjan Maji <debanjanmaji700@gmail.com>
2026-01-17 06:45:42 +0000

    ci: ignore E402 globally instead of using noqa comment

--- backends/generators/c_header/generate_encoding.py
+++ backends/generators/c_header/generate_encoding.py
@@ -14,7 +14,7 @@ parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(parent_dir)
 
 # Import functions from generator.py
-from generator import (  # noqa: E402
+from generator import (
     load_csrs,
     load_exception_codes,
     load_instructions,
