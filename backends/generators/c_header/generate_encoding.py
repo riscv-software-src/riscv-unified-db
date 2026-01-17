4678ac9d1430841e3d814ae628b1665729675071
Debanjan Maji <debanjanmaji700@gmail.com>
2026-01-17 08:05:27 +0000

    ci: inline sys.path.append to avoid E402 instead of ignoring it

--- backends/generators/c_header/generate_encoding.py
+++ backends/generators/c_header/generate_encoding.py
@@ -10,8 +10,7 @@ import os
 import sys
 
 # Add parent directory to path to import generator.py
-parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(parent_dir)
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Import functions from generator.py
 from generator import (
